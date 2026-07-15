dc836f046e64ee1504655dd10dccd0d1a1685fd2
baratavaat-wq <baratavaat@gmail.com>
2026-07-15 18:02:13 -0300

    Update LAUDO_REDE.bat

--- LAUDO_REDE.bat
+++ LAUDO_REDE.bat
@@ -71,7 +71,7 @@ set "LIMITE_INTERNET=120"
 ::           10.1.1.1 / 192.168.1.10 / 172.16.0.1 / srv.local
 ::   MUDA  : LIMITE = ms para bipar como ping alto
 :: +=======================================================+
-set "SERVIDOR=10.1.1.1"
+set "SERVIDOR=10.1.1.7"
 set "LIMITE_SERVIDOR=120"
 
 
